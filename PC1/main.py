0445267f8cce88bac3859cc19d45d043136b02f6
bardend <ricardo.ulloa.v@uni.pe>
2023-09-21 23:05:17 -0500

    xd

--- PC1/main.py
+++ PC1/main.py
@@ -6,7 +6,7 @@ import time
 class EspacioTime:
     def __init__(self, coords):
         self.coords = coords
-        self.tiempo_ocupado = 0
+        self.tiempo_inicio = 0 
         self.tiempo_acumulado = 0
         self.ocupado = False
 
@@ -26,29 +26,36 @@ while check:
      break
     imgBN = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     imgTH = cv2.adaptiveThreshold(imgBN, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
-    imgMedian = cv2.medianBlur(imgTH, 5)
+
+    imgMedian = cv2.medianBlur(imgTH, 7)
+    #kernel = np.ones((6,6), np.int8)
     kernel = np.ones((5,5), np.int8)
     imgDil = cv2.dilate(imgTH, kernel)
     count_persons = 0
-    for rect in espacios_time:
+    time_now = time.time()
+    for ind,rect in enumerate(espacios_time):
         x, y, w, h = rect.coords
         espacio = imgDil[y:y+h, x:x+w]
         total_espacio = espacio.size
         count = cv2.countNonZero(espacio)
         #cv2.putText(img, str(count), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1)
-        t = rect.tiempo_ocupado%60  
+
         #cv2.putText(img, str(t), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1)
+
+        if ind == 0:
+            print(rect.tiempo_acumulado)
+
+
         if count < 3900:#4100:
             if rect.ocupado:
-                rect.tiempo_acumulado += t
+                rect.tiempo_acumulado += time_now-rect.tiempo_inicio
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-            rect.tiempo_ocupado = 0
             rect.ocupado = False
+
         else:
             if not rect.ocupado:
-                rect.tiempo_ocupado = time.time()
+                rect.tiempo_inicio = time_now
             cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-            rect.tiempo_ocupado = time.time() - rect.tiempo_ocupado
             rect.ocupado = True
         if rect.ocupado:
             count_persons += 1
@@ -61,11 +68,10 @@ while check:
     #cv2.imshow('video Dilatada', imgDil)
     cv2.waitKey(10)
 
+time_now = time.time()
 for e in espacios_time:
     if e.ocupado:
-        e.tiempo_acumulado += e.tiempo_ocupado%60
+        e.tiempo_acumulado += time_now - e.tiempo_inicio
     
-i = 1
-for e in espacios_time:
-    print("Tiempo acumulado para el espacio {} es : ".format(i),e.tiempo_acumulado)
-    i += 1
+for i,e in enumerate(espacios_time):
+    print("Tiempo acumulado en segundos para el espacio {} es : ".format(i+1),e.tiempo_acumulado)
