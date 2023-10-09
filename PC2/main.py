3238207dbbae8f536a60213cb37e6be9527e9cce
DagmarLV <dagmar.lezama.v@uni.pe>
2023-10-09 01:17:03 -0500

    PC2 adding files

---
+++ PC2/main.py
@@ -0,0 +1,63 @@
+import tempfile
+import os
+from flask import Flask, request, redirect, send_file, render_template
+from skimage import io
+import base64
+import glob
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        # check if the post request has the file part
+        img_data = request.form.get('myImage').replace("data:image/png;base64,","")
+        aleatorio = request.form.get('numero')
+        print(aleatorio)
+        with tempfile.NamedTemporaryFile(delete = False, mode = "w+b", suffix='.png', dir=str(aleatorio)) as fh:
+            fh.write(base64.b64decode(img_data))
+        #file = request.files['myImage']
+        print("Image uploaded")
+    except Exception as err:
+        print("Error occurred")
+        print(err)
+
+    return redirect("/", code=302)
+
+
+@app.route('/prepare', methods=['GET'])
+def prepare_dataset():
+    images = []
+    d = ["owo", "unu", "uwu","7u7"]
+    digits = []
+    for digit in d:
+      filelist = glob.glob('{}/*.png'.format(digit))
+      images_read = io.concatenate_images(io.imread_collection(filelist))
+      images_read = images_read[:, :, :, 3]
+      digits_read = np.array([digit] * images_read.shape[0])
+      images.append(images_read)
+      digits.append(digits_read)
+    images = np.vstack(images)
+    digits = np.concatenate(digits)
+    np.save('X.npy', images)
+    np.save('y.npy', digits)
+    return "OK!"
+
+@app.route('/X.npy', methods=['GET'])
+def download_X():
+    return send_file('./X.npy')
+@app.route('/y.npy', methods=['GET'])
+def download_y():
+    return send_file('./y.npy')
+
+if __name__ == "__main__":
+    digits = ["owo", "unu", "uwu","7u7"];
+    for d in digits:
+        if not os.path.exists(str(d)):
+            os.mkdir(str(d))
+    app.run(debug=True)
